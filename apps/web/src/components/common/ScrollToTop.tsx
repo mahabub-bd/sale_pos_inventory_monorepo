954142dc2fe5fdd36f3a83ff294d1ca9b34f8019
mahabub-bd <palashmahabub>
2026-08-11 22:11:14 +0600

    Fix ScrollToTop import to use react-router-dom instead of react-router

--- apps/web/src/components/common/ScrollToTop.tsx
+++ apps/web/src/components/common/ScrollToTop.tsx
@@ -1,16 +1,23 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export function ScrollToTop() {
-  const { pathname } = useLocation();
+  const [isClient, setIsClient] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  }, [pathname]);
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [location, isClient]);
 
   return null;
 }
